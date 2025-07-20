60c2104be03cbebb13f9e1095ae9f8c2f7ac773e
aldensd-1211 <aldendcunha1211@gmail.com>
2025-07-20 23:47:48 +0530

    add new user and create tasks

---
+++ create_task.php
@@ -0,0 +1,56 @@
+<?php 
+session_start();
+if (isset($_SESSION['role']) && isset($_SESSION['id']) && $_SESSION['role'] == "admin") {
+    include "DB_connection.php";
+    include "app/Model/User.php";
+
+    $users = get_all_users($conn);
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create Task</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	<input type="checkbox" id="checkbox">
+	<?php include "inc/header.php" ?>
+	<div class="body">
+		<?php include "inc/nav.php" ?>
+		<section class="section-1">
+			<h4 class="title">Create Task </h4>
+		   <form class="form-1"
+			      method="POST"
+			      action="app/add-task.php">
+			      <?php if (isset($_GET['error'])) {?>
+      	  	<div class="danger" role="alert">
+			  <?php echo stripcslashes($_GET['error']); ?>
+			</div>
+      	  <?php } ?>
+
+      	  <?php if (isset($_GET['success'])) {?>
+      	  	<div class="success" role="alert">
+			  <?php echo stripcslashes($_GET['success']); ?>
+			</div>
+      	  <?php } ?>
+				
+			</form>
+			
+		</section>
+	</div>
+
+<script type="text/javascript">
+	var active = document.querySelector("#navList li:nth-child(3)");
+	active.classList.add("active");
+</script>
+</body>
+</html>
+<?php }else{ 
+   $em = "First login";
+   header("Location: login.php?error=$em");
+   exit();
+}
+ ?>
